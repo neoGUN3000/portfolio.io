aa46c9be3ec332f830d4924217e291b487f741cb
neoGUN3000 <oluksijsergiiovic@gmail.com>
2024-07-31 13:06:43 -0500

    bg animation

--- js/main.js
+++ js/main.js
@@ -1,48 +1,49 @@
 
-let nav_buttons = document.getElementsByName('scroll_btn');
+let nav_buttons = document.getElementsByClassName('navigation-btn');
+let nav_buttonsArr = Array.from(nav_buttons);
+
 let pages = document.querySelectorAll('.page');
+let pagesArr = Array.from(pages);
+
 let heading = document.querySelectorAll('.heading');
-// называть множиство множиством 
-
-
-
-// _____mouse___on____HEADING____
-
-for(let i = 0; i < heading.length; i++){
-  heading[i].addEventListener('click', () => {
-    heading[i].scrollIntoView({ behavior: 'smooth' })} );
-
-}
 
 
 
 
 // ______________CLICK__BTN__SCROLL___TO__PAGE___
 
-for(let i = 0; i < nav_buttons.length; i++){
-  nav_buttons[i].addEventListener('click', scrollIntoViewport);
+for(let i = 0; i < nav_buttonsArr.length; i++){
+  nav_buttonsArr[i].addEventListener('click', () => {
+    pagesArr[i].scrollIntoView({ behavior: 'smooth'});
+  });
 }
-window.addEventListener('DOMContentLoaded', scrollIntoViewport());
 
-function  scrollIntoViewport(){
-for(let i = 0; i < nav_buttons.length; i++)
-    if(nav_buttons[i].checked){
-       
-      pages[i].scrollIntoView({ behavior: 'smooth' });
-      
-    }
+pagesArr.forEach(page => {
+  page.addEventListener('mouseover', () => { 
+   if(page =  true){
+    
+    
+   }else{
+    alert('doesn work');
+   }
+  });
+  console.log(page);
+});
+
+
+
+for (let i = 0; i < pagesArr.length; i++) {
+  pagesArr[i].addEventListener('mouseover', () =>{
+      nav_buttonsArr[i].classList.add('gggg');
+  });
+  nav_buttonsArr[i].classList.remove('gggg');
 }
+
+
 // ____________BLACK____BTN_____WHEN_____PAGE___IS___INTO___VIEW__
 
 
 
-for(let i = 0; i < pages.length; i++){
-  pages[i].addEventListener('mouseover', () => {
-    nav_buttons[i].checked = true;
-    
-    });
-
-}
 let get_started = document.getElementById('get_started').addEventListener('click', () => {
   pages[1].scrollIntoView({ behavior: 'smooth'});
 })
